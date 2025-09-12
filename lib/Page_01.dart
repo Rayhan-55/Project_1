763a4392a0692af228277968fa28b42442007713
Rayhan-55 <asfyrayhan1106@gmail.com>
2025-09-12 21:11:47 +0600

    Project_1

---
+++ lib/Page_01.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class Page01 extends StatefulWidget {
+  const Page01({super.key});
+
+  @override
+  State<Page01> createState() => _Page01State();
+}
+
+class _Page01State extends State<Page01> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("This is new "),),
+      body: Center(
+
+      ),
+    );
+  }
+}
